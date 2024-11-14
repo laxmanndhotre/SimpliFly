bfda8eaa84cd449d955759facac2d7d78506b92a
laxmanndhotre <117575876+laxmanndhotre@users.noreply.github.com>
2024-11-14 23:16:55 +0530

    made project

---
+++ simpli_fly/src/main/java/com/hexaware/simplifly/SimpliFlyApplication.java
@@ -0,0 +1,13 @@
+package com.hexaware.simplifly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpliFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpliFlyApplication.class, args);
+	}
+
+}
---
+++ simpli_fly/src/test/java/com/hexaware/simplifly/SimpliFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.simplifly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpliFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

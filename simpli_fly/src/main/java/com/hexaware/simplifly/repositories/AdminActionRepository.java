d7ed704eb8e016ccfff8eeb697f59732548860a8
laxmanndhotre <117575876+laxmanndhotre@users.noreply.github.com>
2024-11-15 22:53:32 +0530

    made repositories

---
+++ simpli_fly/src/main/java/com/hexaware/simplifly/repositories/AdminActionRepository.java
@@ -0,0 +1,9 @@
+package com.hexaware.simplifly.repositories;
+
+import com.hexaware.simplifly.entities.AdminAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminActionRepository extends JpaRepository<AdminAction, Integer> {
+    // Additional methods if needed
+}
+

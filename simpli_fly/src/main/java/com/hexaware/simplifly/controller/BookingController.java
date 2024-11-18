403c8d8f7adde5ec9dc2b7530fbe5a2f69f3a580
trishasahay12 <trishasahay12@gmail.com>
2024-11-18 11:57:05 +0530

    update controller

--- simpli_fly/src/main/java/com/hexaware/simplifly/controller/BookingController.java
+++ simpli_fly/src/main/java/com/hexaware/simplifly/controller/BookingController.java
@@ -1,14 +1,21 @@
 package com.hexaware.simplifly.controller;
 
-import com.hexaware.simplifly.entities.Booking;
-import com.hexaware.simplifly.services.IBookingService;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.hexaware.simplifly.entities.Booking;
+import com.hexaware.simplifly.services.IBookingService;
 
 @RestController
 @RequestMapping("/api/bookings")
@@ -17,12 +24,12 @@ public class BookingController {
     @Autowired
     private IBookingService bookingService;
 
-    @PostMapping("/")
+    @PostMapping("/new")
     public ResponseEntity<Booking> addBooking(@RequestBody Booking booking) {
         return new ResponseEntity<>(bookingService.createBooking(booking), HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/find/id/{id}")
     public ResponseEntity<Booking> getBookingById(@PathVariable int id) {
         Optional<Booking> booking = bookingService.getBookingById(id);
         return booking.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
@@ -33,7 +40,7 @@ public class BookingController {
         return ResponseEntity.ok(bookingService.getBookingsByUserId(userId));
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> cancelBooking(@PathVariable int id) {
         bookingService.cancelBooking(id);
         return ResponseEntity.noContent().build();
--- simpli_fly/src/main/java/com/hexaware/simplifly/controller/UserController.java
+++ simpli_fly/src/main/java/com/hexaware/simplifly/controller/UserController.java
@@ -1,14 +1,20 @@
 package com.hexaware.simplifly.controller;
 
-import com.hexaware.simplifly.entities.User;
-import com.hexaware.simplifly.services.IUserService;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Optional;
+import com.hexaware.simplifly.entities.User;
+import com.hexaware.simplifly.services.IUserService;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,7 +28,7 @@ public class UserController {
         return new ResponseEntity<>(userService.registerUser(user), HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/find/{id}")
     public ResponseEntity<User> getUserById(@PathVariable int id) {
         Optional<User> user = userService.getUserById(id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
@@ -40,7 +46,7 @@ public class UserController {
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
